2cf0e92837ea1adab8d63e776f22ddf642bb2bce
prachipatilcloud <misspatil.04@gmail.com>
2025-09-12 00:10:17 +0530

    Add routing and layout components; update dependencies and styles
    - Implemented routing with `react-router-dom` and created `Home`, `Navbar`, and `Sidebar` components.
    - Added logo image and updated `index.html` to include Material Symbols font.
    - Updated `package.json` and `package-lock.json` to include new dependencies: `autoprefixer`, `postcss`, and `react-router-dom`.
    - Removed unused `react.svg` and cleaned up CSS styles in `index.css` and `App.css`.

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom"
+
+export const Sidebar = () => {
+
+    const getStyles = ({isActive}) => {
+
+        const styles = 'flex align-center gap-1 px-2 py-1 rounded-tr-full rounded-br-full'
+
+
+        return isActive ? `text-slate-50 bg-indigo-800 ${styles}` : `hover:bg-indigo-800 hover:text-slate-50 ${styles} `
+    }
+
+
+    return (
+        <aside className="flex flex-col gap-3 p-3 border-r-2 border-gray-100 w-[220px] h-screen ">
+
+            <NavLink className={getStyles} to='/'>
+                <span class="material-symbols-outlined">
+                    home
+                </span>
+                <span>Home</span>
+            </NavLink>
+            <NavLink className={getStyles} to='/archive'>
+                <span class="material-symbols-outlined">
+                    archive
+                </span>
+                <span>Archive</span>
+            </NavLink>
+            <NavLink className={getStyles} to='/important'>
+                <span class="material-symbols-outlined">
+                    label_important
+                </span>
+                <span>Important</span>
+            </NavLink>
+            <NavLink className={getStyles} to='/bin'>
+                <span class="material-symbols-outlined">
+                    delete
+                </span>
+                <span>Bin</span>
+            </NavLink>
+        </aside>
+    )
+}
